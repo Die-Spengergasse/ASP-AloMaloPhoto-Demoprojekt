0c794518dc07c3622ac173f6d4e8dfd7ad5a117b
schrutek <martin@schrutek>
2024-02-22 20:05:24 +0100

    test removed

--- test/Spg.AloMalo.DomainModel.Test/Helpers/DatabaseUtilities.cs
+++ test/Spg.AloMalo.DomainModel.Test/Helpers/DatabaseUtilities.cs
@@ -7,84 +7,84 @@ namespace Spg.AloMalo.DomainModel.Test.Helpers
 { 
     public static class DatabaseUtilities
     {
-        //public static PhotoContext CreateDb()
-        //{
-        //    SqliteConnection connection = new SqliteConnection("Data Source=:memory:");
-        //    connection.Open();
+        public static PhotoContext CreateDb()
+        {
+            SqliteConnection connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
 
-        //    DbContextOptions options = new DbContextOptionsBuilder()
-        //        .UseSqlite(connection)
-        //        .Options;
+            DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlite(connection)
+                .Options;
 
-        //    PhotoContext db = new PhotoContext(options);
-        //    db.Database.EnsureDeleted();
-        //    db.Database.EnsureCreated();
-        //    return db;
-        //}
+            PhotoContext db = new PhotoContext(options);
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            return db;
+        }
 
-        //public static List<Photographer> GetSeedingPhotographers()
-        //{
-        //    return new List<Photographer>()
-        //    {
-        //        new Photographer(
-        //            "Martin",
-        //            "Schrutek",
-        //            new Address("Photo Street 1", "1234", "Photoville", "Photanien"){ State= new() { Name = "NÖ" } }, //, new State("NÖ")
-        //            new PhoneNumber(43, 2222, "258963147"),
-        //            new PhoneNumber(43, 1234, "123456789"),
-        //            new List<EMail>() { new EMail("schrutek@spengergasse.at"), new EMail("schrutek2@spengergasse.at") },
-        //            new EMail("schrutek@spengergasse.at")
-        //        ),
-        //        new Photographer(
-        //            "Klaus",
-        //            "Unger",
-        //            new Address("Photo Street 2", "7985", "Photoville 2", "Photanien 2"){ State= new() { Name = "WIEN" } }, //, new State("NÖ")
-        //            new PhoneNumber(43, 4561, "987654123"),
-        //            new PhoneNumber(43, 1326, "654789321"),
-        //            new List<EMail>() { new EMail("schrutek@spengergasse.at"), new EMail("unger3@spengergasse.at") },
-        //            new EMail("unger@spengergasse.at")
-        //        )
-        //    };
-        //}
+        public static List<Photographer> GetSeedingPhotographers()
+        {
+            return new List<Photographer>()
+            {
+                new Photographer(
+                    "Martin",
+                    "Schrutek",
+                    new Address("Photo Street 1", "1234", "Photoville", "Photanien"){ State= new() { Name = "NÖ" } }, //, new State("NÖ")
+                    new PhoneNumber(43, 2222, "258963147"),
+                    new PhoneNumber(43, 1234, "123456789"),
+                    new List<EMail>() { new EMail("schrutek@spengergasse.at"), new EMail("schrutek2@spengergasse.at") },
+                    new EMail("schrutek@spengergasse.at")
+                ),
+                new Photographer(
+                    "Klaus",
+                    "Unger",
+                    new Address("Photo Street 2", "7985", "Photoville 2", "Photanien 2"){ State= new() { Name = "WIEN" } }, //, new State("NÖ")
+                    new PhoneNumber(43, 4561, "987654123"),
+                    new PhoneNumber(43, 1326, "654789321"),
+                    new List<EMail>() { new EMail("schrutek@spengergasse.at"), new EMail("unger3@spengergasse.at") },
+                    new EMail("unger@spengergasse.at")
+                )
+            };
+        }
 
-        //public static List<Album> GetSeedingAlbums(List<Photographer> photographers)
-        //{
-        //    return new List<Album>()
-        //    {
-        //        new Album("Landschaften", "ewrtzujkjgfzhtdrs asdfgh cd", false, photographers.ElementAt(0), new TimeStampProvider()),
-        //        new Album("Menschen", "daefsrgdtfhjk sdfrgth kjh sd d", false, photographers.ElementAt(0), new TimeStampProvider()),
-        //        new Album("Dinge", "defsrgthzujki jkjhg mjnhbgv ffsdas", false, photographers.ElementAt(1), new TimeStampProvider()),
-        //    };
-        //}
+        public static List<Album> GetSeedingAlbums(List<Photographer> photographers)
+        {
+            return new List<Album>()
+            {
+                new Album("Landschaften", "ewrtzujkjgfzhtdrs asdfgh cd", false, photographers.ElementAt(0), new TimeStampProvider()),
+                new Album("Menschen", "daefsrgdtfhjk sdfrgth kjh sd d", false, photographers.ElementAt(0), new TimeStampProvider()),
+                new Album("Dinge", "defsrgthzujki jkjhg mjnhbgv ffsdas", false, photographers.ElementAt(1), new TimeStampProvider()),
+            };
+        }
 
-        //public static List<Photo> GetSeedingPhotos(List<Photographer> photographers)
-        //{
-        //    return new List<Photo>()
-        //    {
-        //        new Photo("Baum", "addaas fghjkl adefg", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 100, Orientations.Landscape, false, photographers.ElementAt(0)),
-        //        new Photo("Busch", "qweewqeqw dfrgv dsd", DateTime.Now, ImageTypes.Png, new Location(123, 456), 300, 100, Orientations.Landscape, false, photographers.ElementAt(0)),
-        //        new Photo("Berg", "fgdgfdgfd tghzjkjhgf", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 800, Orientations.Portrait, false, photographers.ElementAt(1)),
-        //        new Photo("Bach", "fhjdscsdfghjnhbgvfd", DateTime.Now, ImageTypes.Png, new Location(123, 456), 400, 500, Orientations.Portrait, false, photographers.ElementAt(1)),
-        //        new Photo("Wald", "ertzhjkl,mnbvcd sedgtf", DateTime.Now, ImageTypes.Png, new Location(123, 456), 800, 400, Orientations.Landscape, false, photographers.ElementAt(1)),
-        //        new Photo("Frau", "dfvbnjijuhfds dsfdgh", DateTime.Now, ImageTypes.Png, new Location(123, 456), 800, 400, Orientations.Landscape, false, photographers.ElementAt(0)),
-        //        new Photo("Mann", "wsedrtzujikkjhgvfc ed", DateTime.Now, ImageTypes.Png, new Location(123, 456), 1000, 600, Orientations.Landscape, false, photographers.ElementAt(0)),
-        //        new Photo("Wiese", "oiujztrdefvgh hjuhki", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 1000, Orientations.Portrait, false, photographers.ElementAt(1))
-        //    };
-        //}
+        public static List<Photo> GetSeedingPhotos(List<Photographer> photographers)
+        {
+            return new List<Photo>()
+            {
+                new Photo("Baum", "addaas fghjkl adefg", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 100, Orientations.Landscape, false, photographers.ElementAt(0)),
+                new Photo("Busch", "qweewqeqw dfrgv dsd", DateTime.Now, ImageTypes.Png, new Location(123, 456), 300, 100, Orientations.Landscape, false, photographers.ElementAt(0)),
+                new Photo("Berg", "fgdgfdgfd tghzjkjhgf", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 800, Orientations.Portrait, false, photographers.ElementAt(1)),
+                new Photo("Bach", "fhjdscsdfghjnhbgvfd", DateTime.Now, ImageTypes.Png, new Location(123, 456), 400, 500, Orientations.Portrait, false, photographers.ElementAt(1)),
+                new Photo("Wald", "ertzhjkl,mnbvcd sedgtf", DateTime.Now, ImageTypes.Png, new Location(123, 456), 800, 400, Orientations.Landscape, false, photographers.ElementAt(1)),
+                new Photo("Frau", "dfvbnjijuhfds dsfdgh", DateTime.Now, ImageTypes.Png, new Location(123, 456), 800, 400, Orientations.Landscape, false, photographers.ElementAt(0)),
+                new Photo("Mann", "wsedrtzujikkjhgvfc ed", DateTime.Now, ImageTypes.Png, new Location(123, 456), 1000, 600, Orientations.Landscape, false, photographers.ElementAt(0)),
+                new Photo("Wiese", "oiujztrdefvgh hjuhki", DateTime.Now, ImageTypes.Png, new Location(123, 456), 200, 1000, Orientations.Portrait, false, photographers.ElementAt(1))
+            };
+        }
 
-        //public static List<AlbumPhoto> GetSeedingAlbumPhotos(List<Album> albums, List<Photo> photos)
-        //{
-        //    return new List<AlbumPhoto>()
-        //    {
-        //        new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(0), 1),
-        //        new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(1), 2),
-        //        new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(2), 3),
-        //        new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(3), 4),
-        //        new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(4), 5),
-        //        new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(5), 1),
-        //        new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(6), 2),
-        //        new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(7), 3),
-        //    };
-        //}
-    } 
+        public static List<AlbumPhoto> GetSeedingAlbumPhotos(List<Album> albums, List<Photo> photos)
+        {
+            return new List<AlbumPhoto>()
+            {
+                new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(0), 1),
+                new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(1), 2),
+                new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(2), 3),
+                new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(3), 4),
+                new AlbumPhoto(albums.ElementAt(0), photos.ElementAt(4), 5),
+                new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(5), 1),
+                new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(6), 2),
+                new AlbumPhoto(albums.ElementAt(1), photos.ElementAt(7), 3),
+            };
+        }
+    }
 }
--- test/Spg.AloMalo.DomainModel.Test/PhotoTests.cs
+++ test/Spg.AloMalo.DomainModel.Test/PhotoTests.cs
@@ -11,7 +11,7 @@ public class PhotoTests : IClassFixture<DatabaseFixture>
     [Fact]
     public void OK_Test()
     {
-        Assert.True(false);
+        Assert.True(true);
     }
 
 
@@ -22,71 +22,71 @@ public class PhotoTests : IClassFixture<DatabaseFixture>
     //    _databaseFixture = databaseFixture;
     //}
 
-    //[Fact]
-    //public void Photo_ShouldInsertValidAlbums_WhenListNotNull()
-    //{
-    //    using (PhotoContext db = DatabaseUtilities.CreateDb())
-    //    {
+    [Fact]
+    public void Photo_ShouldInsertValidAlbums_WhenListNotNull()
+    {
+        using (PhotoContext db = DatabaseUtilities.CreateDb())
+        {
 
-    //        // Arrange
-    //        Photographer newPhotographer = DatabaseUtilities.GetSeedingPhotographers()[0];
-    //        Photo newPhoto = new Photo(
-    //            "Test Photo 01",
-    //            "Beschreibung Test Photo 01...",
-    //            DateTime.Now,
-    //            ImageTypes.Png,
-    //            new Location(12, 17),
-    //            400, 800,
-    //            Orientations.Landscape,
-    //            false,
-    //            newPhotographer
-    //        ).AddAlbums(new Album(
-    //            "Test Album 01", "Beschreibung...",
-    //            true,
-    //            newPhotographer,
-    //            new TimeStampProvider()
-    //        ));
+            // Arrange
+            Photographer newPhotographer = DatabaseUtilities.GetSeedingPhotographers()[0];
+            Photo newPhoto = new Photo(
+                "Test Photo 01",
+                "Beschreibung Test Photo 01...",
+                DateTime.Now,
+                ImageTypes.Png,
+                new Location(12, 17),
+                400, 800,
+                Orientations.Landscape,
+                false,
+                newPhotographer
+            ).AddAlbums(new Album(
+                "Test Album 01", "Beschreibung...",
+                true,
+                newPhotographer,
+                new TimeStampProvider()
+            ));
 
-    //        newPhoto.Name = "asdasdasdasdasdaasdsdadas";
+            newPhoto.Name = "asdasdasdasdasdaasdsdadas";
 
-    //        // Act
-    //        db.Photos.Add(newPhoto);
-    //        db.SaveChanges();
+            // Act
+            db.Photos.Add(newPhoto);
+            db.SaveChanges();
 
-    //        // Assert
-    //        Assert.Equal(1, db.Albums.Count());
-    //        Assert.Equal(1, db.Photos.Count());
-    //        Assert.Single(db.Albums.First().AlbumPhotos);
-    //    }
-    //}
+            // Assert
+            Assert.Equal(1, db.Albums.Count());
+            Assert.Equal(1, db.Photos.Count());
+            Assert.Single(db.Albums.First().AlbumPhotos);
+        }
+    }
 
-    //[Fact]
-    //public void Photo_ShouldNOTInsertValidAlbums_WhenListIsNull()
-    //{
-    //    using (PhotoContext db = DatabaseUtilities.CreateDb())
-    //    {
-    //        // Arrange
-    //        Photographer newPhotographer = DatabaseUtilities.GetSeedingPhotographers()[0];
-    //        Photo newPhoto = new Photo(
-    //            "Test Photo 01",
-    //            "Beschreibung Test Photo 01...",
-    //            DateTime.Now,
-    //            ImageTypes.Png,
-    //            new Location(12, 17),
-    //            400, 800,
-    //            Orientations.Landscape,
-    //            false,
-    //            newPhotographer
-    //        ).AddAlbums(
-    //            null!
-    //        );
+    [Fact]
+    public void Photo_ShouldNOTInsertValidAlbums_WhenListIsNull()
+    {
+        using (PhotoContext db = DatabaseUtilities.CreateDb())
+        {
+            // Arrange
+            Photographer newPhotographer = DatabaseUtilities.GetSeedingPhotographers()[0];
+            Photo newPhoto = new Photo(
+                "Test Photo 01",
+                "Beschreibung Test Photo 01...",
+                DateTime.Now,
+                ImageTypes.Png,
+                new Location(12, 17),
+                400, 800,
+                Orientations.Landscape,
+                false,
+                newPhotographer
+            ).AddAlbums(
+                null!
+            );
 
-    //        // Act
-    //        db.Photos.Add(newPhoto);
-    //        db.SaveChanges();
+            // Act
+            db.Photos.Add(newPhoto);
+            db.SaveChanges();
 
-    //        // Assert
-    //        Assert.Equal(0, db.Albums.Count());
-    //    }
-    //}
+            // Assert
+            Assert.Equal(0, db.Albums.Count());
+        }
+    }
 }
