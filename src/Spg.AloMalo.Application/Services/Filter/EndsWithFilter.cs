7e1a0bc59be39749de10f50fd5fa6ef2ab8079ef
Peter Malinowski <113600140+PeterMalinowski@users.noreply.github.com>
2024-05-23 10:44:32 +0200

    S.O.L.I.D

---
+++ src/Spg.AloMalo.Application/Services/Filter/EndsWithFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AloMalo.Application.Services.Filter
+{
+    public class EndsWithFilter<T> : IFilterOperation<string>
+    {
+        public Func<string, bool> GetFilter(string propertyValue)
+        {
+            return item => item.EndsWith(propertyValue);
+        }
+    }
+}
---
+++ src/Spg.AloMalo.Application/Services/Filter/FilterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Spg.AloMalo.Application.Services.Filter
+{
+    public static class FilterFactory
+    {
+        public static IFilterOperation<T> CreateFilterOperation<T>(string operation) where T : IComparable<T>
+        {
+            switch (operation.ToLower())
+            {
+                case "equals":
+                    return new EqualsFilter<T>();
+                case "startswith":
+                    return (IFilterOperation<T>)new StartsWithFilter<T>();
+                case "endswith":
+                    return (IFilterOperation<T>)new StartsWithFilter<T>();
+                case "greaterthan":
+                    return new GreaterThanFilter<T>();
+                default:
+                    throw new ArgumentException($"Unsupported operation: {operation}");
+            }
+        }
+    }
+}
