d525a27546692f5fd5026d70620f07db95029c92
Matteo <tri22397@spengergasse.at>
2024-05-29 18:53:21 +0200

    Small fix Signed-off-by: Matteo Tripolt

--- test/Spg.AloMalo.DomainModel.Test/FilterTest.cs
+++ test/Spg.AloMalo.DomainModel.Test/FilterTest.cs
@@ -12,6 +12,173 @@ namespace Spg.AloMalo.DomainModel.Test
 	public class FilterTest
 	{
         [Fact]
+        public void DescriptionContainsFilterTest()
+        {
+            using (PhotoContext db = DatabaseUtilities.CreateDb())
+            {
+                // Arrange
+                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
+                Photo newPhoto1 = new Photo(
+                    new Guid("11111111-1111-1111-1111-111111111111"),
+                    "Test Photo 01",
+                    "Beschreibung Test Photo 01...",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 800,
+                    false,
+                    newPhotographer1
+                ).AddAlbums(new Album(
+                    "Test Album 01", "Beschreibung...",
+                    true,
+                    newPhotographer1,
+                    new TimeStampProvider()
+                ));
+                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
+                Photo newPhoto2 = new Photo(
+                    new Guid("22222222-2222-2222-2222-222222222222"),
+                    "Yess Test Photo 02",
+                    "Beschreibung Test Photo 01 blabla...",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 850,
+                    false,
+                    newPhotographer2
+                ).AddAlbums(new Album(
+                    "Test Album 02", "Beschreibung...",
+                    true,
+                    newPhotographer2,
+                    new TimeStampProvider()
+                ));
+
+                // Act
+                db.Photos.Add(newPhoto1);
+                db.Photos.Add(newPhoto2);
+                db.SaveChanges();
+
+                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description ct blabla", ""));
+                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
+
+                var result = handler.Handle(query, CancellationToken.None).Result;
+
+                Assert.Equal(1, result.Count());
+
+            }
+        }
+
+        [Fact]
+        public void DescriptionEndswithFilterTest()
+        {
+            using (PhotoContext db = DatabaseUtilities.CreateDb())
+            {
+                // Arrange
+                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
+                Photo newPhoto1 = new Photo(
+                    new Guid("11111111-1111-1111-1111-111111111111"),
+                    "Test Photo 01",
+                    "Beschreibung Test Photo 01...",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 800,
+                    false,
+                    newPhotographer1
+                ).AddAlbums(new Album(
+                    "Test Album 01", "Beschreibung...",
+                    true,
+                    newPhotographer1,
+                    new TimeStampProvider()
+                ));
+                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
+                Photo newPhoto2 = new Photo(
+                    new Guid("22222222-2222-2222-2222-222222222222"),
+                    "Yess Test Photo 02",
+                    "Beschreibung Test Photo 01 blabla",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 850,
+                    false,
+                    newPhotographer2
+                ).AddAlbums(new Album(
+                    "Test Album 02", "Beschreibung...",
+                    true,
+                    newPhotographer2,
+                    new TimeStampProvider()
+                ));
+
+                // Act
+                db.Photos.Add(newPhoto1);
+                db.Photos.Add(newPhoto2);
+                db.SaveChanges();
+
+                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description ew blabla", ""));
+                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
+
+                var result = handler.Handle(query, CancellationToken.None).Result;
+
+                Assert.Equal(1, result.Count());
+
+            }
+        }
+
+        [Fact]
+        public void DescriptionStartsWithFilterTest()
+        {
+            using (PhotoContext db = DatabaseUtilities.CreateDb())
+            {
+                // Arrange
+                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
+                Photo newPhoto1 = new Photo(
+                    new Guid("11111111-1111-1111-1111-111111111111"),
+                    "Test Photo 01",
+                    "Beschreibung Test Photo 01...",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 800,
+                    false,
+                    newPhotographer1
+                ).AddAlbums(new Album(
+                    "Test Album 01", "Beschreibung...",
+                    true,
+                    newPhotographer1,
+                    new TimeStampProvider()
+                ));
+                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
+                Photo newPhoto2 = new Photo(
+                    new Guid("22222222-2222-2222-2222-222222222222"),
+                    "Yess Test Photo 02",
+                    "Test Beschreibung Test Photo 01 blabla",
+                    DateTime.Now,
+                    ImageTypes.Png,
+                    new Location(12, 17),
+                    400, 850,
+                    false,
+                    newPhotographer2
+                ).AddAlbums(new Album(
+                    "Test Album 02", "Beschreibung...",
+                    true,
+                    newPhotographer2,
+                    new TimeStampProvider()
+                ));
+
+                // Act
+                db.Photos.Add(newPhoto1);
+                db.Photos.Add(newPhoto2);
+                db.SaveChanges();
+
+                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description sw Test", ""));
+                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
+
+                var result = handler.Handle(query, CancellationToken.None).Result;
+
+                Assert.Equal(1, result.Count());
+
+            }
+        }
+        [Fact]
         public void NameContainsFilterTest()
         {
             using (PhotoContext db = DatabaseUtilities.CreateDb())
@@ -237,7 +404,7 @@ namespace Spg.AloMalo.DomainModel.Test
 
                 var result = handler.Handle(query, CancellationToken.None).Result;
 
-                Assert.Equal(1, result.Count());
+                Assert.Equal(2, result.Count());
 
             }
         }
@@ -295,7 +462,7 @@ namespace Spg.AloMalo.DomainModel.Test
 
                 var result = handler.Handle(query, CancellationToken.None).Result;
 
-                Assert.Equal(1, result.Count());
+                Assert.Equal(2, result.Count());
 
             }
         }
@@ -353,7 +520,7 @@ namespace Spg.AloMalo.DomainModel.Test
 
                 var result = handler.Handle(query, CancellationToken.None).Result;
 
-                Assert.Equal(1, result.Count());
+                Assert.Equal(2, result.Count());
 
             }
         }
@@ -410,7 +577,7 @@ namespace Spg.AloMalo.DomainModel.Test
 
                 var result = handler.Handle(query, CancellationToken.None).Result;
 
-                Assert.Equal(1, result.Count());
+                Assert.Equal(2, result.Count());
 
             }
         }
@@ -467,180 +634,11 @@ namespace Spg.AloMalo.DomainModel.Test
 
                 var result = handler.Handle(query, CancellationToken.None).Result;
 
-                Assert.Equal(1, result.Count());
-
-            }
-        }
-
-
-
-        [Fact]
-        public void DescriptionContainsFilterTest()
-        {
-            using (PhotoContext db = DatabaseUtilities.CreateDb())
-            {
-                // Arrange
-                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
-                Photo newPhoto1 = new Photo(
-                    new Guid("11111111-1111-1111-1111-111111111111"),
-                    "Test Photo 01",
-                    "Beschreibung Test Photo 01...",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 800,
-                    false,
-                    newPhotographer1
-                ).AddAlbums(new Album(
-                    "Test Album 01", "Beschreibung...",
-                    true,
-                    newPhotographer1,
-                    new TimeStampProvider()
-                ));
-                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
-                Photo newPhoto2 = new Photo(
-                    new Guid("22222222-2222-2222-2222-222222222222"),
-                    "Yess Test Photo 02",
-                    "Beschreibung Test Photo 01 blabla...",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 850,
-                    false,
-                    newPhotographer2
-                ).AddAlbums(new Album(
-                    "Test Album 02", "Beschreibung...",
-                    true,
-                    newPhotographer2,
-                    new TimeStampProvider()
-                ));
-
-                // Act
-                db.Photos.Add(newPhoto1);
-                db.Photos.Add(newPhoto2);
-                db.SaveChanges();
-
-                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description ct blabla", ""));
-                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
-
-                var result = handler.Handle(query, CancellationToken.None).Result;
-
-                Assert.Equal(1, result.Count());
-
-            }
-        }
-
-        [Fact]
-        public void DescriptionEndswithFilterTest()
-        {
-            using (PhotoContext db = DatabaseUtilities.CreateDb())
-            {
-                // Arrange
-                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
-                Photo newPhoto1 = new Photo(
-                    new Guid("11111111-1111-1111-1111-111111111111"),
-                    "Test Photo 01",
-                    "Beschreibung Test Photo 01...",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 800,
-                    false,
-                    newPhotographer1
-                ).AddAlbums(new Album(
-                    "Test Album 01", "Beschreibung...",
-                    true,
-                    newPhotographer1,
-                    new TimeStampProvider()
-                ));
-                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
-                Photo newPhoto2 = new Photo(
-                    new Guid("22222222-2222-2222-2222-222222222222"),
-                    "Yess Test Photo 02",
-                    "Beschreibung Test Photo 01 blabla",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 850,
-                    false,
-                    newPhotographer2
-                ).AddAlbums(new Album(
-                    "Test Album 02", "Beschreibung...",
-                    true,
-                    newPhotographer2,
-                    new TimeStampProvider()
-                ));
-
-                // Act
-                db.Photos.Add(newPhoto1);
-                db.Photos.Add(newPhoto2);
-                db.SaveChanges();
-
-                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description ew blabla", ""));
-                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
-
-                var result = handler.Handle(query, CancellationToken.None).Result;
-
-                Assert.Equal(1, result.Count());
-
-            }
-        }
-
-        [Fact]
-        public void DescriptionStartsWithFilterTest()
-        {
-            using (PhotoContext db = DatabaseUtilities.CreateDb())
-            {
-                // Arrange
-                Photographer newPhotographer1 = DatabaseUtilities.GetSeedingPhotographers()[0];
-                Photo newPhoto1 = new Photo(
-                    new Guid("11111111-1111-1111-1111-111111111111"),
-                    "Test Photo 01",
-                    "Beschreibung Test Photo 01...",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 800,
-                    false,
-                    newPhotographer1
-                ).AddAlbums(new Album(
-                    "Test Album 01", "Beschreibung...",
-                    true,
-                    newPhotographer1,
-                    new TimeStampProvider()
-                ));
-                Photographer newPhotographer2 = DatabaseUtilities.GetSeedingPhotographers()[1];
-                Photo newPhoto2 = new Photo(
-                    new Guid("22222222-2222-2222-2222-222222222222"),
-                    "Yess Test Photo 02",
-                    "Test Beschreibung Test Photo 01 blabla",
-                    DateTime.Now,
-                    ImageTypes.Png,
-                    new Location(12, 17),
-                    400, 850,
-                    false,
-                    newPhotographer2
-                ).AddAlbums(new Album(
-                    "Test Album 02", "Beschreibung...",
-                    true,
-                    newPhotographer2,
-                    new TimeStampProvider()
-                ));
-
-                // Act
-                db.Photos.Add(newPhoto1);
-                db.Photos.Add(newPhoto2);
-                db.SaveChanges();
-
-                GetPhotosQueryModel query = new GetPhotosQueryModel(new Queries.GetPhotosQuery("description sw Test", ""));
-                GetPhotosQueryHandler handler = new GetPhotosQueryHandler(new PhotoRepository(db, new PhotoFilterBuilder(db.Photos), new PhotoUpdateBuilder(db)));
-
-                var result = handler.Handle(query, CancellationToken.None).Result;
-
-                Assert.Equal(1, result.Count());
+                Assert.Equal(2, result.Count());
 
             }
         }
     }
+
 }
 
